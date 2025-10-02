6235931f59e9663711d8a7779283b7d7bfea20c9
akhil <akhilva10a@gmail.com>
2025-10-02 20:19:10 +0530

    added toggle bar and added poppins font

--- faculty/faculty_dashboard.php
+++ faculty/faculty_dashboard.php
@@ -20,8 +20,7 @@ ob_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <body>
     <header class="dashboard-header">
         <div class="logo">
@@ -31,22 +30,25 @@ ob_start();
     </header>
     <div class="main-body">
         <div class="left-body">
+        <button id="toggleSidebar" class="toggle-btn">
+         <i class="fas fa-bars"></i>
+         </button>
            <a href="?page=fac_home"><span class="home-container font">
              <i class="fas fa-home"></i>
-             Home
+             <span class="label">Home</span>
         </span></a> 
 
          <span class="display-athletes-container font">
             <a href="?page=athletes_info">
                 <i class="fa-solid fa-ribbon"></i>
-                Athletes
+                <span class="label">Athletes</span>
             </a>
             </span>
 
             <div class="select-container">
                  <p class="add-phrase">
                     <i class="fas fa-plus-circle"></i>
-                    Add
+                    <span class="label">Add</span>
                 </p><br><select name="Add-container" class="action-select" value="" >
                     <option value="">-- Select Action --</option>
                 <option value="?page=add_athlete" class="option-addathlete">
@@ -54,9 +56,11 @@ ob_start();
                 </option>
             </select>
             </div>
-             <div class="view-atheletes-container select-container font">
+
+            <div class="menu-item">
+                 <div class="view-atheletes-container select-container font">
                 <i class="fas fa-running"></i>
-                Participants
+                <span class="label">Participants</span>
                 <select name="participants-container" class="action-select" value="" >
                 <option value="">-- Select List --</option>
                 <option value="?page=participants" class="option-inparticipants">
@@ -67,23 +71,28 @@ ob_start();
                 </option>
                 </select>
             </div>
-      <div class="manage-results-container select-container font">
+            </div>
+            
+       <div class="menu-item">
+        <div class="manage-results-container select-container font">
         <i class="fas fa-medal"></i>
-        Results
+        <span class="label">Results</span>
         <select name="results-container" class="action-select">
             <option value="">-- Select List --</option>
             <option value="?page=manage_results">Individual Results</option>
             <option value="?page=manage_relay_results">Relay Results</option>
         </select>
         </div>
+       </div>     
+      
               <span class="Championships-container font"><a href="?page=championships">
                <i class="fas fa-trophy"></i>
-                Championships
+                   <span class="label">Championships</span>
             </a></span>
             <span class="logout-container font">
                 <a href="?page=logout">
                     <i class="fas fa-sign-out-alt"></i>
-                    Logout
+                    <span class="label">Logout</span>
                 </a>
             </span>
         </div>
